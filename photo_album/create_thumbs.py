92646046bc9e411503c84df79b79bd978ab151d6
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2014-07-24 21:16:57 -0400

    Detect EXIF rotation metadata in image
    When making a thumbnail, use the EXIF rotation data (if it exists) to
    rotate the image before making the thumbnail.

--- photo_album/create_thumbs.py
+++ photo_album/create_thumbs.py
@@ -1,5 +1,5 @@
 import os, sys
-from PIL import Image
+from PIL import Image, ExifTags
 
 size = (128, 128)
 
@@ -7,10 +7,26 @@ for infile in os.listdir(sys.argv[1]):
     inpath = os.path.join(sys.argv[1], infile)
     pieces = os.path.splitext(inpath)
     outpath = pieces[0] + ".thumb" + pieces[1]
-    if inpath != outpath and not os.path.exists(outpath):
+    if (inpath != outpath and not os.path.exists(outpath) and
+        'thumb' not in infile):
         try:
-            im = Image.open(inpath)
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(outpath, 'JPEG')
+            image = Image.open(inpath)
+            for orientation in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation] == 'Orientation':
+                    break
+
+            e = image._getexif()
+            if e is not None:
+                exif = dict(e.items())
+                if orientation in exif:
+                    if exif[orientation] == 3:
+                        image=image.transpose(Image.ROTATE_180)
+                    elif exif[orientation] == 6:
+                        image = image.transpose(Image.ROTATE_270)
+                    elif exif[orientation] == 8:
+                        image = image.transpose(Image.ROTATE_90)
+
+            image.thumbnail(size, Image.ANTIALIAS)
+            image.save(outpath, 'JPEG')
         except IOError as ex:
             print('cannot create thumbnail for ' + infile + ' -- ' + ex.strerror)
